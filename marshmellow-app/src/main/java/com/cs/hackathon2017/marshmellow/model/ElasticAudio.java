14df22fcffe73a575e91c18742e46d1e89686906
Jeff <zhangzhuzhefu@gmail.com>
2017-11-06 21:31:38 +0800

    downgrade code to java 8

--- marshmellow-app/src/main/java/com/cs/hackathon2017/marshmellow/model/ElasticAudio.java
+++ marshmellow-app/src/main/java/com/cs/hackathon2017/marshmellow/model/ElasticAudio.java
@@ -7,16 +7,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-<<<<<<< HEAD
-import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.List;
-
-
-
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -32,22 +27,8 @@ public class ElasticAudio {
     private String clientId;
     private String fullText;
 
-//    @Transient
-//    private List<String[]> keywordTimes;
+    @Transient
+    private List<String[]> keywordTimes;
 
-=======
 
-import java.util.List;
-
-@AllArgsConstructor
-@Data
-public class ElasticAudio {
-    final private String id;
-    final private String wavfile;
-    final private String createdTime;
-    final private String rmId;
-    final private String clientId;
-    final private String fullText;
-    final private List<String[]> keywordTimes;
->>>>>>> 00b5a8c165cae6e34c39b2ed7fe78d5727956efb
 }
