64a4bcdd102563f7cf7c7626b3dca6473fa89fa7
raj <torajakumar@gmail.com>
2017-11-06 19:44:20 +0800

    Search API

--- marshmellow-app/src/main/java/com/cs/hackathon2017/marshmellow/api/MarshMellowApi.java
+++ marshmellow-app/src/main/java/com/cs/hackathon2017/marshmellow/api/MarshMellowApi.java
@@ -1,6 +1,7 @@
 package com.cs.hackathon2017.marshmellow.api;
 
-import com.cs.hackathon2017.marshmellow.config.MarshMellowBatchProperties;
+import com.cs.hackathon2017.marshmellow.model.ElasticAudio;
+import com.cs.hackathon2017.marshmellow.service.MarshMellowService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
@@ -10,37 +11,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class MarshMellowApi {
-    private static String NOT_FOUND = "{\"error\":\"not-found\"}";
-
-    private MarshMellowBatchProperties batchProperties;
+    private MarshMellowService service;
 
     @GetMapping("/api/waveform/{fileId}.json")
     @ResponseBody
     public String getWaveform(@PathVariable("fileId") String fileId) {
         log.info("waveform request received {}", fileId);
-
-        try {
-            byte[] waveformJsonData = Files.readAllBytes(Paths.get(batchProperties.getVoiceLogOutput(), fileId + ".json"));
-            return new String(waveformJsonData);
-        } catch (IOException ex) {
-            log.error("Not found: {}", fileId);
-            return NOT_FOUND;
-        }
+        return service.getWaveform(fileId);
     }
 
     @GetMapping(value = "/api/voice/{filename:.+}")
     @ResponseBody
     public FileSystemResource getVoiceLog(@PathVariable("filename") String filename) throws IOException {
         log.info("voice log request received {}", filename);
-        Path voiceFilePath = Paths.get(batchProperties.getVoiceLogOutput(), filename);
-        return new FileSystemResource(voiceFilePath.toFile());
+        return service.getVoiceLogAsResource(filename);
+
+    }
+
+    @GetMapping("/api/search/{keywords}")
+    @ResponseBody
+    public List<ElasticAudio> searchFor(String keyWords) {
+        return service.searchFor(keyWords);
     }
 }
